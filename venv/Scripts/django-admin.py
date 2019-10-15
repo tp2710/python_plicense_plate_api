7481d9750097af874af633598b718eb40b4ea4da
Pham Hoang Tan <tan.pham@netcompany.com>
2019-10-15 13:22:44 +0100

    add source

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\personal\project python\python_plicense_plate_api\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
